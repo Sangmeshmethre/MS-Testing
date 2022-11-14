60f112075e944d595da35e5a77c5af2727504b7c
Sangmesh Methre <SangmeshMetre>
2022-11-14 21:27:36 +0530

    SumOfNumbers

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SumOf2Number
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public int Add(int x,int y)
+        {
+            var sum = x + y;
+            return sum;
+        }
+    }
+}
